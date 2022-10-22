f81499d4d24570cae7641a30b607503890dce97b
Henrique Bortoletto <bortolettohenrique@gmail.com>
2022-10-22 20:48:53 -0300

    feature(git): define limit breaking for title

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 import "./style.css";
 
 document.querySelector("#app").innerHTML = `
-  <h1>FullCycle - Padrões e técnicas avançadas com Git e Github</h1>
+  <h1 class="title">FullCycle - Padrões e técnicas avançadas com Git e Github</h1>
 `;
